c462bf4c5715db4961154ba53c5ab034aaaf1a7b
DaRacci <me@racci.dev>
2023-10-06 18:04:06 +1100

    feat: actually get the remote runner working

--- N-Able/automation/remote-runner.ps1
+++ N-Able/automation/remote-runner.ps1
@@ -1,102 +1,170 @@
 #Requires -Version 5.1
-# Requires -RunAsAdministrator
 
-<#
-.Description
-    This script will download and execute a remote executable from a GitHub repository.
-    The arguments passed to the script are passed to the executable.
-.TODO
-    Implement Hash checking for the downloaded file.
-#>
+Param(
+    [Parameter(Mandatory)]
+    [string]$ProgramName,
 
-# Section start :: Set Variables
+    [Parameter(ValueFromRemainingArguments)]
+    [String[]]$Arguments
+)
 
-$RegexMatcher = "^(?<User>[^:]+):(?<Repository>[^:]+):(?<Version>[^:]+)$"
+# #region - Scope Functions
 
-# Section end :: Set Variables
-
-# Section start :: Classes
-
-Class Repository {
-    [String]$User
-    [String]$Repo
-    [String]$Version
-    [String]$URL
-    [String]$Path
-
-    Repository([String]$User, [String]$Repo, [String]$Version) {
-        $this.User = $User
-        $this.Repo = $Repo
-        $this.Version = $Version
-        $this.URL = "https://api.github.com/repos/${User}/${Repo}/releases/${Version}"
-
-        Write-Host "Repository URL is $($this.URL)"
-    }
-
-    [Void] EnsureLatest() {
-        $request = (Invoke-RestMethod -Uri $this.URL)
-
-        if ($request.assets.Count -eq 0) {
-            Write-Host "No assets found for $($this.User)/$($this.Repo) version $($this.Version)"
-            exit 1
-        }
-
-        $asset = ($request.assets | Where-Object { $_.content_type -eq "application/x-msdownload" })
-        if ($null -eq $asset) {
-            Write-Host "No asset found for $($this.User)/$($this.Repo) version $($this.Version)"
-            exit 1
-        }
-
-        $updatedAt = $asset.updated_at.Replace(':', '-')
-        $assetName = $asset.name.Replace(".exe", '')
-        $assetURL = $asset.browser_download_url
-
-        Write-Host "Downloading $($this.User)/$($this.Repo) version $($this.Version) from $($assetURL)"
-        $this.Path = "$($env:TEMP)\RemoteRunner\$assetName-$updatedAt.exe"
-
-        if (Test-Path -Path $this.Path) {
-            Write-Host "File already exists at $($this.Path)"
-            return
-        }
-
-        if (-not (Test-Path -Path $this.Path)) {
-            Write-Host "Creating directory $($this.Path | Split-Path -Parent)"
-            New-Item -Path ($this.Path | Split-Path -Parent) -ItemType Directory -Force
-        }
-
-        Write-Host "Downloading to $($this.Path)"
-        Invoke-WebRequest -Uri $assetURL -OutFile $this.Path
-    }
-
-    [Void] Execute([String]$parsedArgs = [String]::Empty) {
-        Write-Host "Executing ``$($this.Path)`` with arguments ``$parsedArgs``"
-
-        & "$($this.Path)" "$($parsedArgs)" | ForEach-Object {
-            if ($_ -match 'OK')
-            { Write-Host $_ -f Green }
-            elseif ($_ -match 'FAIL|ERROR')
-            { Write-Host $_ -f Red }
-            else
-            { Write-Host $_ }
-         }
-    }
+function Enter-Scope([System.Management.Automation.InvocationInfo]$Invocation) {
+    $Params = $Invocation.BoundParameters
+    Write-Verbose "Entered scope $($Invocation.MyCommand.Name) with parameters [$($Params.Keys) = $($Params.Values)]"
 }
 
-# Section end :: Classes
-
-if (($args | Where-Object { $_ -match $RegexMatcher }).Count -eq 0) {
-    Write-Host "Invalid arguments. Expected format is ./RemoteRunner.ps1 User:Repository:Version -- [args]"
-    exit 1
+function Exit-Scope([System.Management.Automation.InvocationInfo]$Invocation, [Object]$ReturnValue) {
+    Write-Verbose "Exited scope $($Invocation.MyCommand.Name) with return value [$ReturnValue]"
 }
 
-if ($MyInvocation.Line.Contains(" -- ")) {
-    $parsedArgs = $MyInvocation.Line.SubString($MyInvocation.Line.IndexOf(" -- ") + 4).Trim()
-    Write-host "Parsing additional arguments to executable: ``$parsedArgs``"
-} else {
-    Write-host "Parsing no additional arguments to executable."
+# #endregion Scope Functions
+
+function Get-CachableResponse {
+    begin { Enter-Scope $MyInvocation }
+
+    process {
+        $CachePath = "$env:TEMP/RemoteRunner/cached.json"
+        if (Test-Path -Path $CachePath) {
+            $CacheAge = (Get-Date) - (Get-Item -Path $CachePath).CreationTime
+            if ($CacheAge.Minutes -le 10) {
+                Write-Host "Cache is less than 10 minutes old, skipping api calls and assuming it's the latest version."
+                $Cache = Get-Content -Path $CachePath | ConvertFrom-Json
+                return $Cache
+            }
+        }
+
+        $Url = "https://api.github.com/repos/AMTSupport/tools/releases"
+        Invoke-WebRequest -Uri $Url -OutFile $CachePath -UseBasicParsing | Out-Null
+        return Get-Content -Path $CachePath | ConvertFrom-Json
+    }
+
+    end { Exit-Scope $MyInvocation }
 }
 
-$repo = [Repository]::new($Matches.User, $Matches.Repository, $Matches.Version)
-$repo.EnsureLatest()
-$repo.Execute($parsedArgs)
+function Get-LatestRelease([Parameter(Mandatory)][String]$Program) {
+    begin { Enter-Scope $MyInvocation }
 
+    process {
+        try {
+            # Get the releases for the monorepo
+            $releases = Get-CachableResponse
+        } catch {
+            Write-Error "Failed to get releases for the monorepo."
+            exit 1001
+        }
+
+        try {
+            # Filter the releases to only include those for the desired program
+            $programReleases = $releases | Where-Object { $_.tag_name -like "$ProgramName-v*" }
+
+            # Sort the program releases by date, descending
+            $programReleases = $programReleases | Sort-Object -Property createdAt -Descending
+
+            # Get the latest release of the program
+            $latestProgramRelease = $programReleases[0]
+        } catch {
+            Write-Error "Failed to get the latest release for $ProgramName."
+            exit 1002
+        }
+
+        return $latestProgramRelease
+    }
+
+    end { Exit-Scope $MyInvocation }
+}
+
+function Get-ExecutableArtifact([Parameter(Mandatory)][Object]$Release) {
+    begin { Enter-Scope $MyInvocation }
+
+    process {
+        # Get the Executable Suffix for the current system
+        $Architecture = switch ($Env:PROCESSOR_ARCHITECTURE) {
+            "AMD64" { "x86_64" }
+            "x86" { "i686" }
+            default { Write-Error "Unknown processor architecture: $Env:PROCESSOR_ARCHITECTURE"; exit 1101 }
+        }
+
+        $OperatingSystem = switch ($Env:OS) {
+            "Windows_NT" { "windows" }
+            default { Write-Error "Unknown operating system: $Env:OS"; exit 1102 }
+        }
+
+        # Get the executable artifact for the current system
+        $artifacts = $latestProgramRelease.assets
+
+        # Search for an artifact with the name of the program, the architecture, and the operating system, use like to allow for .exe ext
+        $executableArtifact = $artifacts | Where-Object { $_.name -like "$ProgramName-$Architecture-$OperatingSystem*" }
+        if (!$executableArtifact) {
+            Write-Error "Could not find an executable artifact for the current system."
+            exit 1103
+        }
+
+        return $executableArtifact
+    }
+
+    end { Exit-Scope $MyInvocation }
+}
+
+# TODO :: Caching
+function Get-DownloadedExecutable([Parameter(Mandatory)][Object]$Artifact) {
+    begin { Enter-Scope $MyInvocation }
+
+    process {
+        $Parent = "$env:TEMP\RemoteRunner"
+        $ExecutablePath = "$Parent\$($Artifact.name)"
+
+        $Parent = Split-Path -Path $ExecutablePath -Parent
+        if (-not (Test-Path -Path $Parent)) {
+            Write-Verbose "Creating directory $Parent"
+            New-Item -Path $Parent -ItemType Directory | Out-Null
+        }
+
+        try {
+            $DownloadLink = $Artifact.browser_download_url
+            Invoke-WebRequest -Uri $downloadLink -OutFile $ExecutablePath -UseBasicParsing | Out-Null
+        } catch {
+            Write-Error "Failed to download the latest release of $ProgramName."
+            exit 1201
+        }
+
+        return $ExecutablePath
+    }
+
+    end { Exit-Scope $MyInvocation }
+}
+
+function Invoke-Executable([Parameter(Mandatory)][String]$Path, [String[]]$Arguments) {
+    begin { Enter-Scope $MyInvocation }
+
+    process {
+        try {
+            if ($Arguments.Count -eq 0 -or $null -eq $Arguments) {
+                Start-Process -FilePath $Path -Wait -NoNewWindow
+            } else {
+                Start-Process -FilePath $Path -ArgumentList $Arguments -Wait -NoNewWindow
+            }
+        } catch {
+            Write-Error "Failed to execute ``$($Path)``"
+            Write-Error $_
+            exit 1301
+        }
+    }
+
+    end { Exit-Scope $MyInvocation }
+}
+
+function Main {
+    $latestProgramRelease = Get-LatestRelease -Program $ProgramName
+    $executableArtifact = Get-ExecutableArtifact -Release $latestProgramRelease
+    $executablePath = Get-DownloadedExecutable -Artifact $executableArtifact
+    Invoke-Executable -Path $executablePath -Arguments $Arguments
+}
+
+try {
+    Main
+} catch {
+    Write-Error "Caught unhandled exception" -Exception $_
+    exit 1401
+}
