523a8f9599c906195826c5d454df10cdf83a4d93
DaRacci <me@racci.dev>
2023-10-06 18:05:58 +1100

    feat: ping tester util script

---
+++ N-Able/utilities/ping_tester.ps1
@@ -0,0 +1,43 @@
+Param(
+    [Parameter(ValueFromRemainingArguments)]
+    [string[]]$Adresses
+)
+
+while ($true) {
+    $start = Get-Date
+    $Adresses | Foreach-Object {
+        $result = [ordered]@{
+            DNSName    = $_.Trim()
+            Up         = ""
+            Timestamp  = (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
+        }
+        Try {
+            [array]$x = Test-Connection $result.DNSName -Delay 15 -Count 1 -ErrorAction SilentlyContinue
+            if ($x) {
+                $Output = "$($result.Timestamp): Reply from $($result.DNSName) time=$($x[0].ResponseTime)ms"
+            }
+            else {
+                $Output = "$($result.Timestamp): Reply from $($result.DNSName) timed out."
+            }
+        }
+        Catch {
+            $result.Up = "Unknown"
+        }
+
+        $LogFilePath = "$env:TEMP\ping_tester.log"
+        if ((Test-Path -Path $LogFilePath) -eq $false) {
+            New-Item -Path $LogFilePath -ItemType File
+        }
+
+        $Output | Out-File -FilePath $LogFilePath -Append
+
+        $Output
+    }
+
+    $duration = New-TimeSpan -Start $start -End (Get-Date)
+    if ($duration.Milliseconds -lt 1000) {
+        $sleep = 1000 - $duration.Milliseconds
+        Write-Verbose "Sleeping for $sleep milliseconds"
+        Start-Sleep -Milliseconds $sleep
+    }
+}
