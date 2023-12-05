48263e68f37ad24312ac2911caf4fc9bf8cad5ca
DaRacci <me@racci.dev>
2023-12-04 20:34:54 -0800

    feat: finish rewrite of setup script.
    * Needs "compiling" with due to moving itself and so on as it uses modules.
    * Moved some more functions out to modules for easier maintnance.
    * Updated some variables to use locals and types where possible.
    * Actually supply the error records to quit functions for better error handling.
    * Update logging to use cyan for standard outputs.
    * Fix an issue with selecting site would have infinite recursion.
    * Better usage of flags.

---
+++ src/common/Flag.psm1
@@ -0,0 +1,114 @@
+class Flag {
+    [String][ValidateNotNull()]$Context;
+    [String][ValidateNotNull()]$FlagPath;
+
+    Flag([String]$Context) {
+        $this.Context = $Context;
+        $this.FlagPath = Get-FlagPath -Context $Context;
+    }
+
+    [Boolean]Exists() {
+        return Test-Path $this.FlagPath;
+    }
+
+    [Object]GetData() {
+        if (-not $this.Exists()) {
+            return $null;
+        }
+
+        return Get-Content -Path $this.FlagPath;
+    }
+
+    [Void]Set([Object]$Data) {
+        New-Item -ItemType File -Path $this.FlagPath -Force;
+
+        if ($null -ne $Data) {
+            $Data | Out-File -FilePath $this.FlagPath -Force;
+        }
+    }
+
+    [Void]Remove() {
+        if (-not $this.Exists()) {
+            return
+        }
+
+        Remove-Item -Path $this.FlagPath -Force;
+    }
+}
+
+class RunningFlag: Flag {
+    RunningFlag() : base('running') {}
+
+    [Void]Set([Object]$Data) {
+        if ($Data) {
+            Write-Warning -Message "Data is ignored for RunningFlag, only the PID of the running process is stored."
+        }
+
+        [Int]$Local:CurrentPID = [System.Diagnostics.Process]::GetCurrentProcess().Id;
+        if (-not (Get-Process -Id $Local:CurrentPID -ErrorAction SilentlyContinue)) {
+            throw "PID $Local:CurrentPID is not a valid process";
+        }
+
+        ([Flag]$this).Set($Local:CurrentPID);
+    }
+
+    [Boolean]IsRunning() {
+        if (-not $this.Exists()) {
+            return $false;
+        }
+
+        # Check if the PID in the running flag is still running, if not, remove the flag and return false;
+        [Int]$Local:RunningPID = $this.GetData();
+        if (-not (Get-Process -Id $Local:RunningPID -ErrorAction SilentlyContinue)) {
+            $this.Remove();
+            return $false;
+        }
+
+        return $true;
+    }
+}
+
+class RebootFlag: Flag {
+    RebootFlag() : base('reboot') {}
+
+    [Boolean]Required() {
+        if (-not $this.Exists()) {
+            return $false;
+        }
+
+        # Get the write time for the reboot flag file; if it was written before the computer started, we have reboot, return false;
+        [DateTime]$Local:RebootFlagTime = (Get-Item $this.FlagPath).LastWriteTime;
+        [DateTime]$Local:StartTime = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -ExpandProperty LastBootUpTime;
+
+        return $Local:RebootFlagTime -gt $Local:StartTime;
+    }
+}
+
+function Get-FlagPath(
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [String]$Context
+) {
+    process {
+        # TODO - Make this dynamic based on the calling script's name
+
+        [String]$Local:FlagFolder = "$($env:TEMP)\Flags";
+        if (-not (Test-Path $Local:FlagFolder)) {
+            Write-Host "Creating flag folder $Local:FlagFolder...";
+            New-Item -ItemType Directory -Path $Local:FlagFolder;
+        }
+
+        [String]$Local:FlagPath = "$Local:FlagFolder\$Context.flag";
+        $Local:FlagPath
+    }
+}
+
+function Get-RebootFlag {
+    [RebootFlag]::new();
+}
+
+function Get-RunningFlag {
+    [RunningFlag]::new();
+}
+
+Export-ModuleMember -Function Get-FlagPath,Get-RebootFlag,Get-RunningFlag;
