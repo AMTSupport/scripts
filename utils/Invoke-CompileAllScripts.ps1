bbeedb8e0038d39a260c5a5caf4d822753de743a
DaRacci <me@racci.dev>
2024-01-22 14:18:51 +1100

    feat: add github workflow to compile all scripts into directory.

--- utils/Invoke-CompileAllScripts.ps1
+++ utils/Invoke-CompileAllScripts.ps1
@@ -4,7 +4,11 @@ param (
     [String]$SourceDir = "./src",
 
     [Parameter(HelpMessage='The directory to output compiled scripts to.')]
-    [String]$OutputDir = "./compiled"
+    [String]$OutputDir = "./compiled",
+
+    [Parameter(DontShow, HelpMessage='The compiler scripts location.')]
+    [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+    [String]$CompilerScript = "$PSScriptRoot/Compiler.ps1"
 )
 
 # Function to create directory structure
@@ -23,38 +27,37 @@ function Invoke-EnsureDirectoryStructure {
         [String]$CurrentPath
     )
 
-    [String]$Local:RelativePath = $CurrentPath.Substring($SourcePath.Length)
-    [String]$Local:TargetPath = Join-Path $TargetBasePath $Local:RelativePath
+    [String]$RelativePath = $CurrentPath.Substring((Get-Item $SourcePath).FullName.Length).TrimStart('\');
+    [String]$Local:TargetPath = Join-Path $TargetBasePath $Local:RelativePath;
+
+    Invoke-Info "Creating directory structure for $($Local:TargetPath)";
 
     if (-not (Test-Path -Path $Local:TargetPath)) {
-        New-Item -ItemType Directory -Path $Local:TargetPath | Out-Null
+        New-Item -ItemType Directory -Path $Local:TargetPath | Out-Null;
     }
 }
 
-# Recursively create directory structure and compile scripts
-function Invoke-CompileScripts {
-    param (
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$SourceDir,
+Import-Module $PSScriptRoot/../src/common/Environment.psm1;
+Invoke-RunMain $MyInvocation {
+    Invoke-Info "Compiling scripts from $SourceDir to $OutputDir";
 
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [String]$OutputDir
-    )
-
-    [Object[]]$Local:Items = Get-ChildItem -Path $SourceDir -Recurse
-
+    [Object[]]$Local:Items = Get-ChildItem -Path $SourceDir -Recurse -Filter '*.ps1';
     foreach ($Local:Item in $Local:Items) {
-        if ($Local:Item.PSIsContainer) {
-            Invoke-EnsureDirectoryStructure -SourcePath $SourceDir -TargetBasePath $OutputDir -CurrentPath $Local:Item.FullName
-        } else {
-            [String]$OutputFolderPath = Join-Path $OutputDir $Local:Item.DirectoryName.Substring($SourceDir.Length)
-
-            # Run the compiler command
-            .\src\Onefile-Runner.ps1 -CompileScripts $Local:Item.FullName -Output $OutputFolderPath
+        [String]$Local:Content = Get-Content -Path $Local:Item.FullName;
+        if (($Local:Content.Length -eq 0) -or (Select-String -InputObject $Local:Content -Pattern '^\s*#.*@compile-ignore')) {
+            Invoke-Info "Ignoring $($Local:Item.FullName)";
+            continue;
         }
+
+        if (-not (Test-Path -Path $Local:Item.DirectoryName)) {
+            Invoke-EnsureDirectoryStructure -SourcePath $SourceDir -TargetBasePath $OutputDir -CurrentPath $Local:Item.FullName;
+        }
+
+        Invoke-Info "Compiling $($Local:Item.FullName)";
+        # Get the relative path of the file
+        [String]$Local:RelativePath = $Local:Item.DirectoryName.Substring((Get-Item $SourceDir).FullName.Length).TrimStart('\');
+        [String]$Local:OutputFolderPath = Join-Path $OutputDir $Local:RelativePath;
+
+        & "${CompilerScript}" -CompileScripts "$($Local:Item.FullName)" -Output "${Local:OutputFolderPath}" -Force -InnerInvocation | Out-Null;
     }
 }
-
-Invoke-CompileScripts -SourceDir $SourceDir -OutputDir $OutputDir
