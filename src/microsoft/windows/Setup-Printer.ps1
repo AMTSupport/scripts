c76ee9780175ecc017ea1c60312643a51a0e59b7
DaRacci <me@racci.dev>
2024-01-23 13:06:36 +1100

    refactor: printer setup and logs

--- src/microsoft/windows/Setup-Printer.ps1
+++ src/microsoft/windows/Setup-Printer.ps1
@@ -17,7 +17,13 @@ Param(
         If the driver is not installed already and isn't being installed by chocolatey,
         A 5 minute timeout will be used to wait for the driver to be installed asychronously.
     ")]
-    [String]$ChocoDriver
+    [String]$ChocoDriver,
+
+    [Parameter(HelpMessage = "
+        If specified, printer will be added even if the computer cannot contact the printer.
+        If not specified, if the computer cannot contact the printer, the script will silently exit.
+    ")]
+    [Switch]$Force
 )
 
 function Install-Driver(
@@ -32,21 +38,16 @@ function Install-Driver(
     end { Exit-Scope -Invocation $MyInvocation; }
 
     process {
-        Trap {
-            Write-Host -ForegroundColor Red -Object "Encountered an error while trying to install/find printer driver $DriverName.";
-            throw $_;
-        }
-
         if (-not $ChocolateyPackage) {
-            Write-Host -ForegroundColor Cyan -Object 'No chocolatey package specified, trying to find driver already installed.';
+            Invoke-Info 'No chocolatey package specified, trying to find driver already installed.';
 
             [TimeSpan]$Local:WaitTimeout = New-TimeSpan -Minutes 5;
             do {
-                Write-Verbose -Message "Waiting for driver $DriverName to be installed; $($Local:WaitTimeout.TotalSeconds) seconds remaining...";
+                Invoke-Info "Waiting for driver $DriverName to be installed; $($Local:WaitTimeout.TotalSeconds) seconds remaining...";
                 [DateTime]$Local:StartTime = Get-Date;
 
                 if (Get-PrinterDriver -Name $DriverName -ErrorAction SilentlyContinue) {
-                    Write-Host -ForegroundColor Cyan -Object "Driver $DriverName found, proceeding...";
+                    Invoke-Info "Driver $DriverName found, proceeding...";
                     return;
                 }
 
@@ -55,19 +56,16 @@ function Install-Driver(
                 $Local:WaitTimeout -= $Local:ProcessingTime;
 
                 if ($Local:ProcessingTime.Milliseconds -lt 1000) {
-                    Write-Verbose -Message "Processing time took less than 1 second, sleeping for the remainder of the second.";
+                    Invoke-Verbose "Processing time took less than 1 second, sleeping for the remainder of the second.";
                     Start-Sleep -Milliseconds (1000 - $Local:ProcessingTime.Milliseconds);
                 } else {
-                    Write-Verbose -Message "Wait timeout took longer than 1 second, skipping sleep.";
+                    Invoke-Verbose "Wait timeout took longer than 1 second, skipping sleep.";
                 }
             } while ($Local:WaitTimeout.TotalSeconds -gt 0)
 
             throw "Unable to find driver $DriverName";
         } else {
-            Import-Module "$($env:SystemDrive)\ProgramData\Chocolatey\Helpers\chocolateyProfile.psm1" -Force
-            refreshenv | Out-Null
-
-            choco install -y -e --limit-output $ChocolateyPackage;
+            Install-Package -PackageName $ChocolateyPackage;
         }
     }
 }
@@ -89,36 +87,41 @@ function Install-PrinterImpl(
     end { Exit-Scope -Invocation $MyInvocation; }
 
     process {
-        Trap {
-            Write-Host -ForegroundColor Red -Object "Encountered an error while trying to install printer $PrinterName.";
-            throw $_;
+        if (-not (Get-PrinterPort -Name $PrinterIP -ErrorAction SilentlyContinue)) {
+            Invoke-Info "Adding printer port $PrinterIP";
+            Add-PrinterPort -Name $PrinterIP -PrinterHostAddress $PrinterIP;
+        } else {
+            Invoke-Info "Printer port $PrinterIP already exists.";
         }
 
         if (-not (Get-PrinterPort -Name $PrinterIP -ErrorAction SilentlyContinue)) {
-            Write-Host -ForegroundColor Cyan -Object "Adding printer port $PrinterIP";
+            Invoke-Info "Adding printer port $PrinterIP";
             Add-PrinterPort -Name $PrinterIP -PrinterHostAddress $PrinterIP;
         } else {
-            Write-Host -ForegroundColor Cyan -Object "Printer port $PrinterIP already exists.";
-        }
-
-        if (-not (Get-PrinterPort -Name $PrinterIP -ErrorAction SilentlyContinue)) {
-            Write-Host -ForegroundColor Cyan -Object "Adding printer port $PrinterIP";
-            Add-PrinterPort -Name $PrinterIP -PrinterHostAddress $PrinterIP;
-        } else {
-            Write-Host -ForegroundColor Cyan -Object "Printer port $PrinterIP already exists";
+            Invoke-Info "Printer port $PrinterIP already exists";
         }
 
         if (-not (Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue)) {
-            Write-Host -ForegroundColor Cyan -Object "Adding printer $PrinterName";
+            Invoke-Info "Adding printer $PrinterName";
             Add-Printer -Name $PrinterName -DriverName $PrinterDriver -PortName $PrinterIP;
         } else {
-            Write-Host -ForegroundColor Cyan -Object "Printer $PrinterName already exists";
+            Invoke-Info "Printer $PrinterName already exists";
         }
     }
 }
 
 Import-Module $PSScriptRoot/../../common/Environment.psm1;
 Invoke-RunMain $MyInvocation {
-    Install-Driver -DriverName $PrinterDriver -ChocolateyPackage $ChocoDriver;
-    Install-PrinterImpl -PrinterName $PrinterName -PrinterIP $PrinterIP -PrinterDriver $PrinterDriver;
+    [String]$Local:PrinterName = $PrinterName.Trim();
+    [String]$Local:PrinterIP = $PrinterIP.Trim();
+    [String]$Local:PrinterDriver = $PrinterDriver.Trim();
+    [String]$Local:ChocoDriver = $ChocoDriver.Trim();
+
+    if (-not $Force -and (-not (Test-Connection -ComputerName $Local:PrinterIP -Count 1 -Quiet))) {
+        Invoke-Info "Unable to contact printer $Local:PrinterIP, exiting.";
+        return;
+    }
+
+    Install-Driver -DriverName $Local:PrinterDriver -ChocolateyPackage $Local:ChocoDriver;
+    Install-PrinterImpl -PrinterName $Local:PrinterName -PrinterIP $Local:PrinterIP -PrinterDriver $Local:PrinterDriver;
 }
