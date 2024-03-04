027e38fecb529f1a6c7519c980d0517335198b42
DaRacci <me@racci.dev>
2024-03-04 17:37:05 +1100

    feat: disable fastboot automation script

---
+++ src/automation/Invoke-DisableFastStartup.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    Disables Windows Fast Boot, also known as Hiberboot or Fast Startup.
+.DESCRIPTION
+    In this day and age, realistically Fast Boot causes more issues than it solves,
+    the time saving is less than a matter of seconds.
+    Disabling Fast Boot will keep the system healther and more reliable in day to day use.
+.EXAMPLE
+    Invoke-DisableFastStartup
+.OUTPUTS
+    None
+.NOTES
+    This script requires administrative privileges to run and will exit without them.
+    This script is idempotent, it can be run multiple times without causing issues.
+.FUNCTIONALITY
+    System, Windows, Power, Boot
+#>
+
+Import-Module $PSScriptRoot/../common/00-Environment.psm1;
+Invoke-RunMain $MyInvocation {
+    Invoke-EnsureAdministrator;
+
+    Set-RegistryKey `
+        -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' `
+        -Key 'HiberbootEnabled' `
+        -Value 0 `
+        -Kind DWord;
+};
