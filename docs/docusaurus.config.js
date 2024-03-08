de1cf23d9c49160a2f80981a45a2a4ec3fe8e76d
DaRacci <me@racci.dev>
2024-03-08 19:03:07 +1100

    chore: work on documentation, not done yet.

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,96 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: "AMT Scripts",
+    tagline: "Powershell is not fun, it is pure pain.",
+    favicon: "img/favicon.jpg",
+
+    // Set the production url of your site here
+    url: "https://github.com",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/AMTSupport/scripts",
+
+    // GitHub pages deployment config.
+    organizationName: "AMTSupport",
+    projectName: "Scripts",
+
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
+
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: "./sidebars.js",
+                    editUrl:
+                        "https://github.com/AMTSupport/scripts/tree/master/docs",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: "img/docusaurus-social-card.jpg",
+            navbar: {
+                title: "My Site",
+                logo: {
+                    alt: "My Site Logo",
+                    src: "img/logo.png",
+                },
+                items: [
+                    {
+                        type: "docSidebar",
+                        sidebarId: "ModulesSidebar",
+                        position: "left",
+                        label: "Modules",
+                    },
+                    // {
+                    //     type: "docSidebar",
+                    //     sidebarId: "ScriptsSidebar",
+                    //     position: "left",
+                    //     label: "Scripts",
+                    // },
+                    {
+                        href: "https://github.com/AMTSupport/scripts",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [],
+                copyright: `Copyright © ${new Date().getFullYear()} Applied Marketing Technologies.`,
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
+};
+
+export default config;
