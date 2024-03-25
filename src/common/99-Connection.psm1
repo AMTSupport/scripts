8e14b2e2ddb23b56eb7fc740eb140c657795bd20
DaRacci <me@racci.dev>
2024-03-25 12:11:28 +1100

    refactor: check scopes for graph and disconnect properly

--- src/common/99-Connection.psm1
+++ src/common/99-Connection.psm1
@@ -1,3 +1,4 @@
+[Int]$Script:ERROR_NOT_CONNECTED = Register-ExitCode -Description 'Not connected to {0}, must be connected to continue.';
 function Connect-Service(
     [Parameter(Mandatory)]
     [ValidateSet('ExchangeOnline', 'SecurityComplience', 'AzureAD', 'Graph', 'Msol')]
@@ -6,15 +7,13 @@ function Connect-Service(
     [Parameter()]
     [String[]]$Scopes,
 
-    # If true prompt for confirmation if already connected.
+    # If true don't prompt for confirmation if already connected.
     [Switch]$DontConfirm
 ) {
     foreach ($Local:Service in $Services) {
         Invoke-Info "Connecting to $Local:Service...";
 
         $Local:Connected = try {
-            $ErrorActionPreference = 'SilentlyContinue'; # For some reason AzureAD loves to be noisy.
-
             switch ($Service) {
                 'ExchangeOnline' {
                     Get-ConnectionInformation | Select-Object -ExpandProperty UserPrincipalName;
@@ -26,28 +25,75 @@ function Connect-Service(
                     Get-AzureADCurrentSessionInfo | Select-Object -ExpandProperty Account;
                 }
                 'Graph' {
-                    Get-MgDomain | Where-Object { $_.IsDefault -eq $True } | Select-Object -First 1 -ExpandProperty Id;
+                    $Local:Context = Get-MgContext;
+                    if ($null -eq $Local:Context) {
+                        $null;
+                    } elseif ($Scopes) {
+                        [Bool]$Local:HasAllScopes = ($Local:Context.Scopes | Where-Object { $Scopes -notcontains $_; }).Count -eq 0;
+                        if ($Local:HasAllScopes) {
+                            $Local:Context.Account;
+                        } else {
+                            $null;
+                        }
+                    } else {
+                        $Local:Context.Account;
+                    }
+
+                    if ($Scopes) {
+                        [Bool]$Local:HasAllScopes = (Get-MgContext | Select-Object -ExpandProperty Scopes | Where-Object { $Scopes -notcontains $_; }).Count -eq 0;
+                    } else {
+                        $Local:HasAllScopes = $False;
+                    }
+
+                    if ($Local:HasAllScopes) {
+                        Get-MgContext | Select-Object -ExpandProperty Account;
+                    } else {
+                        Invoke-Warn 'Not all required scopes are present. Reconnecting...';
+                        $null;
+                    }
                 }
                 'Msol' {
                     Get-MsolCompanyInformation | Select-Object -ExpandProperty DisplayName;
                 }
             }
         } catch {
-            $null
+            Invoke-Debug "Failed to get connection information for $Local:Service";
+            if ($Global:Logging.Debug) {
+                Invoke-FormattedError -InvocationInfo $_.InvocationInfo;
+            }
         }
 
         if ($Local:Connected) {
             if (!$DontConfirm) {
                 $Local:Continue = Get-UserConfirmation -Title "Already connected to $Local:Service as [$Local:Connected]" -Question 'Do you want to continue?' -DefaultChoice $true;
                 if ($Local:Continue) {
+                    Invoke-Verbose 'Continuing with current connection...';
                     continue;
                 }
 
-                Invoke-Verbose 'Continuing with current connection...';
+                switch ($Local:Service) {
+                    'ExchangeOnline' {
+                        Disconnect-ExchangeOnline -Confirm:$False;
+                    }
+                    'SecurityComplience' {
+                        Disconnect-IPPSSession;
+                    }
+                    'AzureAD' {
+                        Disconnect-AzureAD;
+                    }
+                    'Graph' {
+                        Disconnect-MgGraph;
+                    }
+                    'Msol' {
+                        Disconnect-MsolService;
+                    }
+                }
             } else {
                 Invoke-Verbose "Already connected to $Local:Service. Skipping..."
                 continue
             }
+        } elseif ($CheckOnly) {
+            Invoke-FailedExit -ExitCode:$Script:ERROR_NOT_CONNECTED -FormatArgs @($Local:Service);
         }
 
         try {
@@ -55,7 +101,7 @@ function Connect-Service(
 
             switch ($Local:Service) {
                 'ExchangeOnline' {
-                    Connect-ExchangeOnline;
+                    Connect-ExchangeOnline -ShowBanner:$False;
                 }
                 'SecurityComplience' {
                     Connect-IPPSSession;
