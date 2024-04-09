69e6f5de9db90022c9bc5013063347da16c144d1
DaRacci <me@racci.dev>
2024-04-09 17:29:44 +1000

    feat: more registry functions

--- src/common/99-Registry.psm1
+++ src/common/99-Registry.psm1
@@ -10,20 +10,71 @@ function Invoke-EnsureRegistryPath {
         [String]$Path
     )
 
-    [String[]]$Local:PathParts = $Path.Split('\') | Where-Object { -not [string]::IsNullOrWhiteSpace($_) };
-    [String]$Local:CurrentPath = "${Root}:";
+    begin { Enter-Scope; }
+    end { Exit-Scope; }
 
-    foreach ($Local:PathPart in $Local:PathParts) {
-        [String]$Local:CurrentPath = Join-Path -Path $Local:CurrentPath -ChildPath $Local:PathPart;
-        if (Test-Path -Path $Local:CurrentPath -PathType Container) {
-            Invoke-Verbose "Registry key '$Local:CurrentPath' already exists.";
-            continue;
+    process {
+        [String[]]$Local:PathParts = $Path.Split('\') | Where-Object { -not [string]::IsNullOrWhiteSpace($_) };
+        [String]$Local:CurrentPath = "${Root}:";
+
+        foreach ($Local:PathPart in $Local:PathParts) {
+            [String]$Local:CurrentPath = Join-Path -Path $Local:CurrentPath -ChildPath $Local:PathPart;
+            if (Test-Path -Path $Local:CurrentPath -PathType Container) {
+                Invoke-Verbose "Registry key '$Local:CurrentPath' already exists.";
+                continue;
+            }
+
+            if ($PSCmdlet.ShouldProcess($Local:CurrentPath, 'Create')) {
+                Invoke-Verbose "Creating registry key '$Local:CurrentPath'...";
+                New-Item -Path $Local:CurrentPath -Force -ItemType RegistryKey;
+            }
+        }
+    }
+}
+
+function Test-RegistryKey {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [String]$Path,
+
+        [Parameter(Mandatory)]
+        [String]$Key
+    )
+
+    begin { Enter-Scope; }
+    end { Exit-Scope; }
+
+    process {
+        if (Test-Path -Path $Path -PathType Container) {
+            if (Get-ItemProperty -Path $Path -Name $Key -ErrorAction SilentlyContinue) {
+                return $True;
+            }
         }
 
-        if ($PSCmdlet.ShouldProcess($Local:CurrentPath, 'Create')) {
-            Invoke-Verbose "Creating registry key '$Local:CurrentPath'...";
-            New-Item -Path $Local:CurrentPath -Force -ItemType RegistryKey;
+        return $False;
+    }
+}
+
+function Get-RegistryKey {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [String]$Path,
+
+        [Parameter(Mandatory)]
+        [String]$Key
+    )
+
+    begin { Enter-Scope; }
+    end { Exit-Scope; }
+
+    process {
+        if (Test-RegistryKey -Path $Path -Key $Key) {
+            return Get-ItemProperty -Path $Path -Name $Key;
         }
+
+        return $Null;
     }
 }
 
@@ -44,11 +95,42 @@ function Set-RegistryKey {
         [Microsoft.Win32.RegistryValueKind]$Kind
     )
 
-    Invoke-EnsureRegistryPath -Root $Path.Substring(0, 4) -Path $Path.Substring(5);
-    if ($PSCmdlet.ShouldProcess($Path, 'Set')) {
-        Invoke-Verbose "Setting registry key '$Path'...";
-        Set-ItemProperty -Path $Path -Name $Key -Value $Value -Type $Kind;
+    begin { Enter-Scope; }
+    end { Exit-Scope; }
+
+    process {
+        Invoke-EnsureRegistryPath -Root $Path.Substring(0, 4) -Path $Path.Substring(5);
+        if ($PSCmdlet.ShouldProcess($Path, 'Set')) {
+            Invoke-Verbose "Setting registry key '$Path'...";
+            Set-ItemProperty -Path $Path -Name $Key -Value $Value -Type $Kind;
+        }
     }
 }
 
-Export-ModuleMember -Function New-RegistryKey, Remove-RegistryKey, Set-RegistryKey, Test-RegistryKey;
+function Remove-RegistryKey {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [String]$Path,
+
+        [Parameter(Mandatory)]
+        [String]$Key
+    )
+
+    begin { Enter-Scope; }
+    end { Exit-Scope; }
+
+    process {
+        if (-not (Test-RegistryKey -Path $Path -Key $Key)) {
+            Invoke-Verbose "Registry key '$Path\$Key' does not exist.";
+            return;
+        }
+
+        if ($PSCmdlet.ShouldProcess($Path, 'Remove')) {
+            Invoke-Verbose "Removing registry key '$Path\$Key'...";
+            Remove-ItemProperty -Path $Path -Name $Key;
+        }
+    }
+}
+
+Export-ModuleMember -Function New-RegistryKey, Remove-RegistryKey, Test-RegistryKey, Get-RegistryKey, Set-RegistryKey;
