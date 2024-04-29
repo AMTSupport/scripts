ad15ab3c4c09e6f80a2863926bcb7108e8791cf9
DaRacci <me@racci.dev>
2024-04-29 16:49:36 +1000

    feat(registry): hive and loop methods
    * Adds methods to get all loaded, unloaded and all sids
    * Method that invokes a scriptblock for each users hive

--- src/common/99-Registry.psm1
+++ src/common/99-Registry.psm1
@@ -26,7 +26,7 @@ function Invoke-EnsureRegistryPath {
 
             if ($PSCmdlet.ShouldProcess($Local:CurrentPath, 'Create')) {
                 Invoke-Verbose "Creating registry key '$Local:CurrentPath'...";
-                New-Item -Path $Local:CurrentPath -Force -ItemType RegistryKey;
+                $null = New-Item -Path $Local:CurrentPath -Force -ItemType RegistryKey;
             }
         }
     }
@@ -101,7 +101,7 @@ function Set-RegistryKey {
     process {
         Invoke-EnsureRegistryPath -Root $Path.Substring(0, 4) -Path $Path.Substring(5);
         if ($PSCmdlet.ShouldProcess($Path, 'Set')) {
-            Invoke-Verbose "Setting registry key '$Path'...";
+            Invoke-Verbose "Setting registry key '$Path' to '$Value'...";
             Set-ItemProperty -Path $Path -Name $Key -Value $Value -Type $Kind;
         }
     }
@@ -133,4 +133,78 @@ function Remove-RegistryKey {
     }
 }
 
-Export-ModuleMember -Function New-RegistryKey, Remove-RegistryKey, Test-RegistryKey, Get-RegistryKey, Set-RegistryKey;
+function Get-AllSIDs {
+    $PatternSID = '(?:S-1-5-21|S-1-12-1)-\d+-\d+\-\d+\-\d+$'
+    Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' `
+        | Where-Object {$_.PSChildName -match $PatternSID} `
+        | Select-Object @{name="SID";expression={$_.PSChildName}},
+             @{name="UserHive";expression={"$($_.ProfileImagePath)\ntuser.dat"}},
+             @{name="Username";expression={$_.ProfileImagePath -replace '^(.*[\\\/])', ''}};
+}
+
+function Get-UnloadedUserHives {
+    param(
+        [Parameter(Mandatory)]
+        [PSCustomObject[]]$LoadedHives,
+
+        [Parameter(Mandatory)]
+        [PSCustomObject[]]$ProfileList
+    )
+
+    Compare-Object $ProfileList.SID $LoadedHives.SID | Select-Object @{name = 'SID'; expression = { $_.InputObject } }, UserHive, Username
+}
+
+function Get-LoadedUserHives {
+    return Get-ChildItem Registry::HKEY_USERS `
+        | Where-Object { $_.PSChildname -match $PatternSID } `
+        | Select-Object @{name = 'SID'; expression = { $_.PSChildName}};
+}
+
+function Invoke-OnEachUserHive {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ScriptBlock]$ScriptBlock
+    )
+
+    begin { Enter-Scope; }
+    end { Exit-Scope; }
+
+    process {
+        [PSCustomObject[]]$Private:ProfileList = Get-AllSIDs;
+        [PSCustomObject[]]$Private:LoadedHives = Get-LoadedUserHives;
+        [PSCustomObject[]]$Private:UnloadedHives = Get-UnloadedUserHives -LoadedHives $Private:LoadedHives -ProfileList $Private:ProfileList;
+
+        Invoke-Debug "Loaded hives: $($Private:LoadedHives.SID -join ', ')";
+        Invoke-Debug "Unloaded hives: $($Private:UnloadedHives.SID -join ', ')";
+
+        Foreach ($Private:Hive in $Private:ProfileList) {
+            Invoke-Verbose "Processing hive '$($Private:Hive)'...";
+
+            $Private:IsUnloadedHive = $Private:Hive.SID -in $Private:UnloadedHives.SID;
+
+            If ($Private:IsUnloadedHive) {
+                Invoke-Debug "Loading hive '$($Private:Hive.UserHive)'...";
+                reg load "HKU\$($Private:Hive.SID)" $Private:Hive.UserHive;
+
+                if (-not (Test-Path -Path Registry::HKEY_USERS\$($Private:Hive.SID))) {
+                    Invoke-Warn "Failed to load hive '$($Private:Hive.UserHive)'.";
+                    continue;
+                }
+            }
+
+            try {
+                Invoke-Debug "Executing script block...";
+                & $ScriptBlock -Hive $Private:Hive;
+            } finally {
+                If ($Private:IsUnloadedHive) {
+                    Invoke-Debug "Unloading hive '$($Private:Hive.UserHive)'...";
+                    [GC]::Collect();
+                    reg unload HKU\$($Private:Hive.SID);
+                }
+            }
+        }
+    }
+}
+
+Export-ModuleMember -Function New-RegistryKey, Remove-RegistryKey, Test-RegistryKey, Get-RegistryKey, Set-RegistryKey, Invoke-OnEachUserHive;
