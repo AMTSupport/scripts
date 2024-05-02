9a63319b5b26be316fa67c48b56b66c9230ea9ed
DaRacci <me@racci.dev>
2024-05-02 13:58:50 +1000

    feat: fix and complete span and span tests for now

--- src/Compiler/Text/Span.cs
+++ src/Compiler/Text/Span.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Text
@@ -38,14 +39,28 @@ namespace Text
         {
             if (StartingIndex == EndingIndex)
             {
+                if (StartingColumn == 0 && EndingColumn == document.Lines[StartingIndex].Length)
+                {
+                    return document.Lines[StartingIndex];
+                }
+
+                if (StartingColumn == EndingColumn)
+                {
+                    return string.Empty;
+                }
+
                 return document.Lines[StartingIndex][StartingColumn..EndingColumn];
             }
 
-            string content = document.Lines[StartingIndex][StartingColumn..];
-            content += Environment.NewLine;
-            content += string.Join(Environment.NewLine, document.Lines[(StartingIndex + 1)..EndingIndex]);
-            content += document.Lines[EndingIndex][..EndingColumn];
-            return content;
+            var builder = new StringBuilder();
+            builder.AppendLine(document.Lines[StartingIndex][StartingColumn..]);
+            for (int i = StartingIndex + 1; i < EndingIndex; i++)
+            {
+                builder.AppendLine(document.Lines[i]);
+            }
+            builder.Append(document.Lines[EndingIndex][..EndingColumn]);
+
+            return builder.ToString();
         }
 
         /// <summary>
@@ -63,48 +78,88 @@ namespace Text
         public int SetContent(TextDocument document, string[] content)
         {
             var offset = 0;
+            var lastLineAfter = document.Lines[EndingIndex][EndingColumn..];
+            var firstLineBefore = document.Lines[StartingIndex][..StartingColumn];
+
             if (StartingIndex == EndingIndex)
             {
-                document.Lines[StartingIndex] = string.Concat(document.Lines[StartingIndex][StartingColumn..], string.Join(Environment.NewLine, content), document.Lines[StartingIndex][..EndingColumn]);
+                // If the span is a single line and we took nothing from the line before or after, remove the line, since it will be empty.
+                if (string.IsNullOrEmpty(firstLineBefore) || string.IsNullOrEmpty(lastLineAfter))
+                {
+                    document.Lines.RemoveAt(StartingIndex);
+                    offset--;
+
+                    // Short circuit if the new content is empty, there will be no need to update the document.
+                    if (content.Length == 0)
+                    {
+                        return offset;
+                    }
+                } else {
+                    document.Lines.RemoveAt(StartingIndex);
+                    offset--;
+                }
             }
             else
             {
-                StringBuilder sb = new StringBuilder();
-                sb.Append(document.Lines[StartingIndex][StartingColumn..]);
-                sb.Append(content[0]);
-
-                document.Lines[StartingIndex] = document.Lines[StartingIndex][..StartingColumn] + content[0];
-
-                for (int i = StartingIndex + 1; i < EndingIndex; i++)
+                // Remove all lines in the span to get a clean slate.
+                for (int i = StartingIndex; i <= EndingIndex; i++)
                 {
-                    document.Lines.RemoveAt(StartingIndex + 1);
+                    document.Lines.RemoveAt(StartingIndex);
                     offset--;
                 }
+            }
 
-                // Skip the first line, since it was already updated.
-                for (int i = 1; i < content.Length; i++)
+            if (content.Length == 0)
+            {
+                if (!string.IsNullOrEmpty(firstLineBefore))
                 {
-                    document.Lines.Insert(StartingIndex + i + 1, content[i]);
+                    document.Lines.Insert(StartingIndex, firstLineBefore);
                     offset++;
                 }
 
-                if (content.Length > 1)
+                if (!string.IsNullOrEmpty(lastLineAfter))
                 {
-                    document.Lines[EndingIndex] = content[^1] + document.Lines[EndingIndex + offset][EndingColumn..];
+                    document.Lines.Insert(EndingIndex + offset, lastLineAfter);
+                    offset++;
                 }
-                else
-                {
-                    document.Lines[EndingIndex] = document.Lines[EndingIndex][EndingColumn..];
+            }
+            else if (content.Length == 1)
+            {
+                if (StartingIndex == EndingIndex) {
+                    document.Lines.Insert(StartingIndex, firstLineBefore + content[0] + lastLineAfter);
+                    offset++;
+                } else {
+                    document.Lines.Insert(StartingIndex, firstLineBefore + content[0]);
+                    offset++;
 
+                    if (!string.IsNullOrEmpty(lastLineAfter))
+                    {
+                        document.Lines.Insert(EndingIndex + offset, lastLineAfter);
+                        offset++;
+                    }
                 }
+            }
+            else
+            {
+                document.Lines.Insert(StartingIndex, firstLineBefore + content[0]);
+                offset++;
+
+                // Add the new content.
+                for (int i = 1; i < content.Length; i++)
+                {
+                    document.Lines.Insert(StartingIndex + i, content[i]);
+                    offset++;
+                }
+
+                document.Lines.Insert(EndingIndex + offset, content[^1] + lastLineAfter);
             }
 
             return offset;
         }
 
-        public void RemoveContent(TextDocument document)
+        public int RemoveContent(TextDocument document)
         {
-            SetContent(document, []);
+            return SetContent(document, []);
         }
     }
 }
--- tests/Compiler/Text/Span.cs
+++ tests/Compiler/Text/Span.cs
@@ -1,17 +1,22 @@
 
+using System.Collections;
+
 namespace Text.Tests
 {
     [TestFixture]
     public class TextSpanTests
     {
+        private string[] lines;
         private TextDocument document;
 
         [SetUp]
         public void SetUp()
         {
-            string[] lines = [
+            lines = [
                 "Hello,",
-                "World!"
+                "World!",
+                "I'm the",
+                "Document!"
             ];
 
             document = new TextDocument(lines);
@@ -28,8 +33,8 @@ namespace Text.Tests
             {
                 Assert.That(span.StartingIndex, Is.EqualTo(0));
                 Assert.That(span.StartingColumn, Is.EqualTo(0));
-                Assert.That(span.EndingIndex, Is.EqualTo(1));
-                Assert.That(span.EndingColumn, Is.EqualTo(6));
+                Assert.That(span.EndingIndex, Is.EqualTo(lines.Length - 1));
+                Assert.That(span.EndingColumn, Is.EqualTo(expected: lines[^1].Length));
             });
         }
 
@@ -42,22 +47,68 @@ namespace Text.Tests
         }
 
         [Test]
-        public void GetContent_ReturnsCorrectContent()
+        public void GetContent_EntireDocument()
         {
             var span = TextSpan.WrappingEntireDocument(document);
             string content = span.GetContent(document);
-            Assert.That(content, Is.EqualTo($"Hello,{Environment.NewLine}World"));
+            Assert.That(content, Is.EqualTo($"Hello,{Environment.NewLine}World!{Environment.NewLine}I'm the{Environment.NewLine}Document!"));
+        }
+
+        [Test]
+        public void GetContent_SingleLineFromBeginingAndEndLines()
+        {
+            var spanFirst = new TextSpan(0, 0, 0, lines[0].Length);
+            var contentFirst = spanFirst.GetContent(document);
+
+            var spanLast = new TextSpan(3, 0, lines.Length - 1, lines[^1].Length);
+            var contentLast = spanLast.GetContent(document);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(contentFirst, Is.EqualTo(lines[0]));
+                Assert.That(contentLast, Is.EqualTo(lines[^1]));
+            });
+        }
+
+        [Test]
+        public void GetContent_SingleLineFromMiddleLines()
+        {
+            var spans = new List<TextSpan>();
+            var contents = new List<string>();
+
+            for (int i = 1; i < lines.Length - 1; i++)
+            {
+                spans.Add(new TextSpan(i, 0, i, lines[i].Length));
+                contents.Add(spans[i - 1].GetContent(document));
+            }
+
+            Assert.Multiple(() =>
+            {
+                for (int i = 0; i < contents.Count; i++)
+                {
+                    Assert.That(contents[i], Is.EqualTo(lines[i + 1]));
+                }
+            });
+        }
+
+        [Test]
+        public void GetContent_MultipleLines()
+        {
+            var span = new TextSpan(1, 0, 2, lines[2].Length);
+            var content = span.GetContent(document);
+
+            Assert.That(content, Is.EqualTo($"World!{Environment.NewLine}I'm the"));
         }
 
         [Test]
         public void SetContent_UpdatesLinesWithNewContent()
         {
             var span = TextSpan.WrappingEntireDocument(document);
-            int linesChanged = span.SetContent(document, ["New Content"]);
+            int lengthChanged = span.SetContent(document, ["New Content"]);
 
             Assert.Multiple(() =>
             {
-                Assert.That(linesChanged, Is.EqualTo(-1));
+                Assert.That(lengthChanged, Is.EqualTo(-3));
                 Assert.That(document.Lines[0], Is.EqualTo("New Content"));
                 Assert.That(document.Lines, Has.Count.EqualTo(1));
             });
@@ -65,20 +116,97 @@ namespace Text.Tests
         }
 
         [Test]
-        public void RemoveContent_RemovesContentFromLines()
-        {
-            var span = new TextSpan(0, 0, 0, 6);
-
-            // Act
-            span.RemoveContent(document);
+        public void SetContent_UpdateMiddleLine() {
+            var span = new TextSpan(1, 0, 1, lines[1].Length);
+            int lengthChanged = span.SetContent(document, ["New Content"]);
 
             Assert.Multiple(() =>
             {
-                // Assert
-                Assert.That(document.Lines[0], Is.EqualTo("World!"));
-                Assert.That(document.Lines, Has.Count.EqualTo(1));
+                Assert.That(lengthChanged, Is.EqualTo(0));
+                Assert.That(document.Lines[1], Is.EqualTo("New Content"));
             });
+        }
 
+        [Test]
+        public void SetContent_UpdateFirstLine() {
+            var span = new TextSpan(0, 0, 0, lines[0].Length);
+            int lengthChanged = span.SetContent(document, ["New Content"]);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(lengthChanged, Is.EqualTo(0));
+                Assert.That(document.Lines[0], Is.EqualTo("New Content"));
+            });
+        }
+
+        [Test]
+        public void SetContent_UpdateLastLine() {
+            var span = new TextSpan(3, 0, 3, lines[^1].Length);
+            int lengthChanged = span.SetContent(document, ["New Content"]);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(lengthChanged, Is.EqualTo(0));
+                Assert.That(document.Lines[3], Is.EqualTo("New Content"));
+            });
+        }
+
+        [Test]
+        public void SetContent_InsertsNewLines()
+        {
+            var span = new TextSpan(1, 0, 1, lines[1].Length);
+            int lengthChanged = span.SetContent(document, ["New", "Content"]);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(lengthChanged, Is.EqualTo(1));
+                Assert.That(document.Lines[1], Is.EqualTo("New"));
+                Assert.That(document.Lines[2], Is.EqualTo("Content"));
+                Assert.That(document.Lines, Has.Count.EqualTo(5));
+            });
+        }
+
+        [Test]
+        public void SetContent_AppendAndPrependColumns()
+        {
+            var spanStart = new TextSpan(0, lines[0].Length - 1, 0, lines[0].Length - 1);
+            int lengthChangedStart = spanStart.SetContent(document, [" beautiful"]);
+            var spanEnd = new TextSpan(lines.Length - 1, 0, lines.Length - 1, 0);
+            int lengthChangedEnd = spanEnd.SetContent(document, ["Awesome "]);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(lengthChangedStart, Is.EqualTo(0));
+                Assert.That(lengthChangedEnd, Is.EqualTo(0));
+                Assert.That(document.Lines[0], Is.EqualTo("Hello beautiful,"));
+                Assert.That(document.Lines[^1], Is.EqualTo("Awesome Document!"));
+            });
+        }
+
+        [Test]
+        public void SetContent_UpdateMiddleOfLines() {
+            var span = new TextSpan(lines.Length - 2, 3, lines.Length - 2, 3);
+            int lengthChanged = span.SetContent(document, [" not"]);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(lengthChanged, Is.EqualTo(0));
+                Assert.That(document.Lines[^2], Is.EqualTo("I'm not the"));
+            });
+        }
+
+        [Test]
+        public void RemoveContent_RemovesContentFromLines()
+        {
+            var span = new TextSpan(startingIndex: 0, 0, 0, 6);
+            var lengthChanged = span.RemoveContent(document);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(lengthChanged, Is.EqualTo(-1));
+                Assert.That(document.Lines[0], Is.EqualTo("World!"));
+                Assert.That(document.Lines, Has.Count.EqualTo(3));
+            });
         }
     }
 }
