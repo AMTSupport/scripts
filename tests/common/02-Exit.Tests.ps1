c829549d6ae11727f0d90439264397ce68aa7236
DaRacci <me@racci.dev>
2024-05-13 11:58:07 +1000

    test: add some more exit tests

--- tests/common/02-Exit.Tests.ps1
+++ tests/common/02-Exit.Tests.ps1
@@ -3,12 +3,13 @@ BeforeDiscovery {
 }
 
 Describe 'Exit Tests' {
-    Context 'Invoke-Exit' {
+    Context 'Invoke-FailedExit' {
         It 'Should throw with a FailedExit ErrorRecord with the ExitCode as the TargetObject' {
             $Local:ThrownError;
             try {
                 Invoke-FailedExit -ExitCode 1;
-            } catch {
+            }
+            catch {
                 $Local:ThrownError = $_;
             }
 
@@ -20,15 +21,29 @@ Describe 'Exit Tests' {
             Invoke-FailedExit -ExitCode 1 -DontExit;
         }
 
-        It 'Should call Invoke-Handlers with IsFailure $true if ExitCode is not 0' {
-            Mock -Verifiable -ModuleName:$ModuleName -CommandName Invoke-Handlers -MockWith { } -ParameterFilter { $IsFailure -eq $True; };
+        It 'Should emit a warning if the ExitCode is not a registered error code' {
+            Mock -ModuleName:$ModuleName -Verifiable -CommandName Invoke-Warn -MockWith { } -ParameterFilter { $Message -eq 'No exit description found for code ''1050''' };
 
-            Invoke-FailedExit -ExitCode 1 -DontExit;
+            try { Invoke-FailedExit -ExitCode 1050 } catch { };
 
-            Should -InvokeVerifiable;
-            Should -Invoke -CommandName Invoke-Handlers -ModuleName:$ModuleName -Times 1;
+            Should -ModuleName:$ModuleName -Invoke -CommandName:'Invoke-Warn' -Times 1;
         }
 
+        It 'Should not emit an error if the ExitCode is registered' {
+
+            Mock -ModuleName:$ModuleName -CommandName Invoke-FailedExit -MockWith { } -ParameterFilter { $ExitCode -eq $ExitCode; };
+
+            try { Invoke-FailedExit -ExitCode $ExitCode } catch { };
+
+            Should -Not -Invoke -CommandName Invoke-Warn;
+        }
+    }
+
+    Context 'Invoke-QuickExit' {
+
+    }
+
+    Context 'Handlers' {
         It 'Should call Invoke-Handlers with IsFailure $false if ExitCode is 0' {
             Mock -Verifiable -ModuleName:$ModuleName -CommandName Invoke-Handlers -MockWith { } -ParameterFilter { $IsFailure -eq $False; };
 
@@ -37,5 +52,20 @@ Describe 'Exit Tests' {
             Should -InvokeVerifiable;
             Should -Invoke -CommandName Invoke-Handlers -ModuleName:$ModuleName -Times 1;
         }
+
+        It 'Should call Invoke-Handlers with IsFailure $true if ExitCode is not 0' {
+            Mock -Verifiable -ModuleName:$ModuleName -CommandName Invoke-Handlers -MockWith { } -ParameterFilter { $IsFailure -eq $True; };
+
+            Invoke-FailedExit -ExitCode $Code -DontExit;
+
+            Should -InvokeVerifiable;
+            Should -Invoke -CommandName Invoke-Handlers -ModuleName:$ModuleName -Times 1;
+        } -ForEach (1..100 | ForEach-Object {
+            @{ Code = $_ }
+        })
+    }
+
+    Context 'Script Tests' {
+
     }
 }
