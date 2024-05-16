8a5c04544e9c4969253f18f294f077fae4b211ec
DaRacci <me@racci.dev>
2024-05-16 10:22:22 +1000

    chore: debugging

--- src/Compiler/Requirements/Requirements.cs
+++ src/Compiler/Requirements/Requirements.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Text;
 using CommandLine;
 
 namespace Compiler.Requirements;
@@ -50,30 +49,6 @@ public class Requirements
 public abstract record Requirement(bool SupportsMultiple)
 {
     public abstract string GetInsertableLine();
-}
-
-public record ModuleSpec(
-    string Name,
-    Guid? Guid = null,
-    Version? MinimumVersion = null,
-    Version? MaximumVersion = null,
-    Version? RequiredVersion = null,
-    ModuleType Type = ModuleType.Downloadable
-) : Requirement(true)
-{
-    public override string GetInsertableLine()
-    {
-        var sb = new StringBuilder("#Requires -Modules @{");
-
-        sb.Append($"ModuleName = '{Name}';");
-        if (Guid != null) sb.Append($"GUID = {Guid};");
-        sb.Append($"ModuleVersion = '{(MinimumVersion != null ? MinimumVersion.ToString() : "0.0.0.0")}';");
-        if (MaximumVersion != null) sb.Append($"MaximumVersion = '{MaximumVersion}';");
-        if (RequiredVersion != null) sb.Append($"RequiredVersion = '{RequiredVersion}';");
-        sb.Append('}');
-
-        return sb.ToString();
-    }
 }
 
 public enum ModuleType
