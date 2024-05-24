239783727ecdca22c63041399991dfb16d54958d
DaRacci <me@racci.dev>
2024-05-24 13:47:25 +1000

    fix(Compiler): PSVersion IsCompatableWith tests
    Add comments to each branch for easier readability.
    Fix some incompatability with base versions.
    Add short circuit for other requirement types.

--- src/Compiler/Requirements/PSVersion.cs
+++ src/Compiler/Requirements/PSVersion.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Compiler.Requirements;
@@ -24,11 +25,28 @@ public record PSVersionRequirement(Version Version) : Requirement(false)
         return sb.ToString();
     }
 
-    // TODO - Check modules for version compatibility
-    public override bool IsCompatibleWith(Requirement other) => (this, other) switch
+    public override bool IsCompatibleWith([NotNull] Requirement other) => (this, other) switch
     {
+        // TODO - Check modules for version compatibility
+        // Short circuit for non-version requirements
+        (_, var otherRequirement) when otherRequirement is not PSVersionRequirement => true,
+
+        // Short circuit for the same version
         (var current, PSVersionRequirement otherVersion) when current.Version.Major == otherVersion.Version.Major => true,
+
+        // General rule of thumb is anything less than 4 is not compatible with anything greater than 4
         (var current, PSVersionRequirement otherVersion) when (otherVersion.Version.Major <= 4 && current.Version.Major > 4) || (otherVersion.Version.Major > 4 && current.Version.Major <= 4) => false,
+
+        // If the other version is greater than the current version, it's compatible
+        (var current, PSVersionRequirement otherVersion) when current.Version.Major < otherVersion.Version.Major => true,
+
+        // If the other version is less than the current version, it's not compatible
+        (var current, PSVersionRequirement otherVersion) when current.Version.Major > otherVersion.Version.Major => false,
+
+        // If it passed all this its probably compatible
+        (_, var otherRequirement) when otherRequirement is PSVersionRequirement => true,
+
+        // This should never happen, but compiler will complain if it's not here
         _ => true
     };
 }
