29c0155778e7fece1cf0b8350585caee025bca7b
DaRacci <me@racci.dev>
2024-05-31 11:57:26 +1000

    chore(Compiler): Still not fully fixed

--- src/Compiler/Program.cs
+++ src/Compiler/Program.cs
@@ -14,7 +14,7 @@ class Program
         public bool Verbose { get; set; }
 
         [Option('i', "input", Required = true, HelpText = "Input file to be processed.")]
-        public string InputFile { get; set; }
+        public string? InputFile { get; set; }
 
         [Option('o', "output", Required = false, HelpText = "Output file to be written.")]
         public string? OutputFile { get; set; }
