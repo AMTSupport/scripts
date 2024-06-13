31e757c4edeac128b7f770624d16cd98cbaf96df
DaRacci <me@racci.dev>
2024-06-13 17:52:58 +1000

    feat: big steps for remote modules and the loading process of them

--- src/Compiler/AstHelper.cs
+++ src/Compiler/AstHelper.cs
@@ -1,16 +1,21 @@
 using System.Management.Automation.Language;
 using CommandLine;
+using NLog;
 
 namespace Compiler
 {
     public static class AstHelper
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public static Dictionary<string, Dictionary<string, object>> FindDeclaredModules(Ast ast)
         {
             var modules = new Dictionary<string, Dictionary<string, object>>();
 
             foreach (var usingStatement in ast.FindAll(testAst => testAst is UsingStatementAst usingAst && usingAst.UsingStatementKind == UsingStatementKind.Module, true))
             {
+                Logger.Debug($"Found module: {usingStatement}");
+
                 switch (usingStatement)
                 {
                     case UsingStatementAst usingStatementAst when usingStatementAst.Name is not null:
@@ -23,7 +28,7 @@ namespace Compiler
                         while (pairs.MoveNext())
                         {
                             var key = pairs.Current.Item1.SafeGetValue().Cast<string>();
-                            var value = pairs.Current.Item2.SafeGetValue().GetType();
+                            var value = pairs.Current.Item2.SafeGetValue();
                             table.Add(key, value);
                         }
 
@@ -34,22 +39,21 @@ namespace Compiler
 
                         foreach (var key in table.Keys)
                         {
-                            if (key == "ModuleName")
-                            {
-                                continue;
-                            }
+                            Logger.Debug($"Key: {key}, Value: {table[key]}");
 
                             if (key == "Guid" && table[key] is string guid)
                             {
                                 table[key] = Guid.Parse(guid);
                             }
 
-                            if (key.EndsWith("Version") && table[key] is string version)
-                            {
-                                table[key] = Version.Parse(version);
-                            }
+                            // if (key.EndsWith("Version") && table[key] is string version)
+                            // {
+                            //     table[key] = Version.Parse(version);
+                            // }
                         }
 
+                        Logger.Debug($"Adding {table["ModuleName"] as string} with value {table}");
+                        modules.Add((table["ModuleName"] as string)!, table);
                         break;
                     default:
                         Console.WriteLine($"Unknown UsingStatementAst type from: {usingStatement}");
