5a594e92746e6d81c078697d5626a47926323d8a
DaRacci <me@racci.dev>
2024-07-03 17:39:14 +1000

    test(Compiler): Some requirement tests

---
+++ tests/Compiler/Requirements/RunAsAdmin.cs
@@ -0,0 +1,42 @@
+using System.Security.Cryptography;
+using System.Text;
+using Compiler.Requirements;
+
+namespace Compiler.Test.Requirements;
+
+[TestFixture]
+public class RunAsAdminRequirementTests {
+    [Test]
+    public void Hash_ShouldReturnHashOfString() {
+        var requirement = new RunAsAdminRequirement();
+        var hash = requirement.Hash;
+
+        Assert.That(hash, Is.EqualTo(SHA1.HashData(Encoding.UTF8.GetBytes("#Requires -RunAsAdministrator"))));
+    }
+
+    [Test]
+    public void Hash_ShouldAlwaysBeTheSame() {
+        var requirement = new RunAsAdminRequirement();
+        var requirement1 = new RunAsAdminRequirement();
+
+        Assert.That(requirement.Hash, Is.EqualTo(requirement1.Hash));
+    }
+
+    [Test]
+    public void GetInsertableLine_ShouldReturnString() {
+        var requirement = new RunAsAdminRequirement();
+
+        var insertableLine = requirement.GetInsertableLine();
+
+        Assert.That(insertableLine, Is.EqualTo("#Requires -RunAsAdministrator"));
+    }
+
+    [Test]
+    public void IsCompatibleWith_ShouldReturnTrue() {
+        var requirement = new RunAsAdminRequirement();
+
+        var isCompatible = requirement.IsCompatibleWith(new RunAsAdminRequirement());
+
+        Assert.That(isCompatible, Is.True);
+    }
+}
