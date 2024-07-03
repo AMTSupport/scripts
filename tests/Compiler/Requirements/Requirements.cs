d08c95c333e78be82096817f780f79bfe877f403
DaRacci <me@racci.dev>
2024-07-03 17:39:14 +1000

    test(Compiler): Some requirement tests

--- tests/Compiler/Requirements/Requirements.cs
+++ tests/Compiler/Requirements/Requirements.cs
@@ -36,3 +36,122 @@ public class RequirementsTests
         }
     }
 }
+
+[TestFixture]
+public class RequirementGroupTests
+{
+    [Test]
+    public void AddRequirement_ShouldAddRequirementToList()
+    {
+        var requirementGroup = new RequirementGroup();
+        var requirement = new SampleRequirement();
+
+        requirementGroup.AddRequirement(requirement);
+
+        var storedRequirements = requirementGroup.GetRequirements<SampleRequirement>();
+        Assert.Multiple(() =>
+        {
+            Assert.That(storedRequirements, Has.Count.EqualTo(1));
+            Assert.That(storedRequirements, Does.Contain(requirement));
+        });
+    }
+
+    [Test]
+    public void RemoveRequirement_ShouldRemoveRequirementFromList()
+    {
+        var requirementGroup = new RequirementGroup();
+        var requirement = new SampleRequirement();
+        requirementGroup.AddRequirement(requirement);
+
+        var removed = requirementGroup.RemoveRequirement(requirement);
+
+        var storedRequirements = requirementGroup.GetRequirements<SampleRequirement>();
+        Assert.Multiple(() =>
+        {
+            Assert.That(removed, Is.True);
+            Assert.That(storedRequirements, Is.Empty);
+            Assert.That(storedRequirements, Does.Not.Contain(requirement));
+        });
+    }
+
+    [Test]
+    public void ReplaceRequirement_ShouldReplaceOldRequirementWithNewRequirement()
+    {
+        var requirementGroup = new RequirementGroup();
+        var oldRequirement = new SampleRequirement();
+        var newRequirement = new SampleRequirement();
+        requirementGroup.AddRequirement(oldRequirement);
+
+        var replaced = requirementGroup.ReplaceRequirement(oldRequirement, newRequirement);
+
+
+        var storedRequirements = requirementGroup.GetRequirements<SampleRequirement>();
+        Assert.Multiple(() =>
+        {
+            Assert.That(replaced, Is.True);
+            Assert.That(storedRequirements, Has.Count.EqualTo(1));
+            Assert.That(storedRequirements, Does.Not.Contain(oldRequirement));
+            Assert.That(storedRequirements, Does.Contain(newRequirement));
+        });
+    }
+
+    [Test]
+    public void GetRequirements_ShouldReturnAllRequirementsInOrder()
+    {
+        var requirementGroup = new RequirementGroup();
+        var requirement1 = new SampleRequirement();
+        var requirement2 = new SampleRequirement();
+        requirementGroup.AddRequirement(requirement1);
+        requirementGroup.AddRequirement(requirement2);
+
+        var requirements = requirementGroup.GetRequirements();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(requirements, Has.Count.EqualTo(2));
+            Assert.That(requirements.ElementAt(0), Is.EqualTo(requirement1));
+            Assert.That(requirements.ElementAt(1), Is.EqualTo(requirement2));
+        });
+
+    }
+
+    [Test]
+    public void VerifyRequirements_ShouldReturnTrueWhenAllRequirementsAreCompatible()
+    {
+        var requirementGroup = new RequirementGroup();
+        var requirement1 = new SampleRequirement();
+        var requirement2 = new SampleRequirement();
+        requirementGroup.AddRequirement(requirement1);
+        requirementGroup.AddRequirement(requirement2);
+
+        var result = requirementGroup.VerifyRequirements();
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void VerifyRequirements_ShouldReturnFalseWhenIncompatibleRequirementsExist()
+    {
+        var requirementGroup = new RequirementGroup();
+        var requirement1 = new SampleRequirement();
+        var requirement2 = new IncompatibleRequirement();
+        requirementGroup.AddRequirement(requirement1);
+        requirementGroup.AddRequirement(requirement2);
+
+        var result = requirementGroup.VerifyRequirements();
+        Assert.That(result, Is.False);
+    }
+
+    private record SampleRequirement() : Requirement(true)
+    {
+        public override byte[] Hash => [];
+        public override bool IsCompatibleWith(Requirement other) => true;
+        public override string GetInsertableLine() => string.Empty;
+    }
+
+    private record IncompatibleRequirement() : Requirement(true)
+    {
+        public override byte[] Hash => [];
+        public override bool IsCompatibleWith(Requirement other) => false;
+        public override string GetInsertableLine() => string.Empty;
+    }
+}
