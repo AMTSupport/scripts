f2a46f206877cda21aec0b4a3b634e7eea091558
DaRacci <me@racci.dev>
2024-07-11 18:47:31 +1000

    chore(Compiler): update to testing script

--- utils/test.ps1
+++ utils/test.ps1
@@ -1,10 +1,14 @@
+#Requires -Version 5.1;
+#Requires -Modules @{ModuleName='PSReadLine';RequiredVersion='2.3.5'};
+
 Using namespace System.Management.Automation
+
 Using module ../src/common/00-Environment.psm1
+Using module ../src/common/01-Logging.psm1
 Using module ../src/common/02-Exit.psm1
-Using module @{
-    ModuleName      = 'PSReadLine';
-    RequiredVersion = '2.3.5';
-}
+Using module ../src/common/50-Input.psm1
+
+Using module 'PSWindowsUpdate'
 
 <#
     Making some random documentation for the module here!!
@@ -17,20 +21,19 @@ param(
 
 Set-StrictMode -Version 3;
 
-Import-Module $PSScriptRoot/../src/common/00-Environment.psm1;
 Invoke-RunMain $MyInvocation {
     Write-Host 'Hello, World!';
 
     # Write-Error 'This is an error message!' -Category InvalidOperation;
-    Invoke-FailedExit 1050;
+    # Invoke-FailedExit 1050;
 
     # Random comment
     $Restart = Get-UserConfirmation 'Restart' 'Do you want to restart the script?';
     if ($Restart) {
-        Write-Host 'Restarting script...';
+        Invoke-Info 'Restarting script...';
         Restart-Script; # Comment at the end of a line!!
     }
     else {
-        Write-Host 'Exiting script...';
+        Invoke-Info 'Exiting script...';
     };
 }
