99314275883a09186e23e9226bc89472c6269a09
DaRacci <me@racci.dev>
2024-08-09 17:32:22 +1000

    perf(Compiler): Cache analyser results

--- src/Compiler/Analyser/Analyser.cs
+++ src/Compiler/Analyser/Analyser.cs
@@ -8,16 +8,32 @@ public static class Analyser
 {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
+
     private static readonly IEnumerable<Rule> Rules = [
         new UseOfUndefinedFunction()
     ];
 
+    private static readonly HashSet<string> Cache = [];
+
     public static void Analyse(CompiledLocalModule module, IEnumerable<Compiled> availableImports)
     {
+        var key = module.ComputedHash;
+        if (availableImports.Any()) key += availableImports.Select(x => x.ComputedHash).Aggregate((x, y) => x + y);
+
+        lock (Cache)
+        {
+            if (Cache.Contains(key)) return;
+        }
+
         Logger.Trace($"Analyzing module {module.ModuleSpec.Name}");
 
         var visitor = new RuleVisitor(Rules, availableImports);
         module.Ast.Visit(visitor);
         visitor.Issues.ForEach(Program.Issues.Add);
+
+        lock (Cache)
+        {
+            Cache.Add(key);
+        }
     }
 }
