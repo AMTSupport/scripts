fbaabcb77869d431f21280b4f151bb0931130336
DaRacci <me@racci.dev>
2024-09-12 15:40:18 +1000

    fix(Compiler): Caching for Analyser

--- src/Compiler/Analyser/Analyser.cs
+++ src/Compiler/Analyser/Analyser.cs
@@ -1,8 +1,10 @@
 // Copyright (c) James Draycott. All Rights Reserved.
 // Licensed under the GPL3 License, See LICENSE in the project root for license information.
 
+using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
 using Compiler.Analyser.Rules;
 using Compiler.Module.Compiled;
 using NLog;
@@ -12,21 +14,25 @@ namespace Compiler.Analyser;
 public static class Analyser {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-
     private static readonly IEnumerable<Rule> Rules = [
         new UseOfUndefinedFunction()
     ];
 
-    private static readonly HashSet<string> Cache = [];
+    private static readonly ConcurrentBag<string> Cache = [];
 
     [Pure]
     [return: NotNull]
     public static List<Issue> Analyse(CompiledLocalModule module, IEnumerable<Compiled> availableImports) {
-        var key = module.ComputedHash;
-        if (availableImports.Any()) key += availableImports.Select(x => x.ComputedHash).Aggregate((x, y) => x + y);
+        var key = module.ComputedHash[0..8];
+        if (availableImports.Any()) {
+            var rawBytes = new List<byte>();
+            availableImports.ToList().ForEach(x => rawBytes.AddRange(Convert.FromHexString(x.ComputedHash)));
+            key += Convert.ToHexString(SHA256.HashData(rawBytes.ToArray()))[0..8];
+        }
 
-        lock (Cache) {
-            if (Cache.Contains(key)) return [];
+        if (Cache.Contains(key)) {
+            Logger.Trace($"Cache hit for {module.ModuleSpec.Name} with key {key}");
+            return [];
         }
 
         Logger.Trace($"Analyzing module {module.ModuleSpec.Name}");
@@ -34,9 +40,8 @@ public static class Analyser {
         var visitor = new RuleVisitor(module, Rules, availableImports);
         module.Document.Ast.Visit(visitor);
 
-        lock (Cache) {
-            _ = Cache.Add(key);
-        }
+        Logger.Trace($"Caching analysis for {module.ModuleSpec.Name} with key {key}");
+        Cache.Add(key);
 
         return visitor.Issues;
     }
