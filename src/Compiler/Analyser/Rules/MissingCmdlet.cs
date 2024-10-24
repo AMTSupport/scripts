6f22782fe2ef01b76377cbbcd6689db9094d7f93
DaRacci <me@racci.dev>
2024-10-24 17:21:14 +1100

    refactor(Analyser): Mark the extent as the usage of PSCmdlet instead of a random location if possible

--- src/Compiler/Analyser/Rules/MissingCmdlet.cs
+++ src/Compiler/Analyser/Rules/MissingCmdlet.cs
@@ -9,6 +9,9 @@ namespace Compiler.Analyser.Rules;
 
 /// <summary>
 /// Ensures that the script contains a param block with the CmdletBinding Attribute at the top level.
+///
+/// This is applied to all scripts as a general rule, however is only important to scripts using the
+/// Invoke-RunMain $PSCmdlet function due to the usage of the $PSCmdlet object.
 /// </summary>
 public class MissingCmdlet : Rule {
     public override bool SupportsModule<T>(T compiledModule) => compiledModule is CompiledScript;
@@ -38,14 +41,27 @@ public class MissingCmdlet : Rule {
     /// Gets the error location for the issue.
     ///
     /// If the script block has a top level param block, then its extent is returned.
-    /// Otherwise an extent covering the location after the using statements,
-    /// but before the first command, function or variable assignment is returned.
+    /// If the script usage of the $PSCmdlet object with the Invoke-RunMain function, then that extent is returned.
+    /// If an extent covering the location after the using statements, but before the first command, function or variable assignment is returned.
     /// </summary>
     /// <param name="scriptBlockAst"></param>
     /// <returns></returns>
     private static IScriptExtent GetErrorLocation(ScriptBlockAst scriptBlockAst) {
         if (scriptBlockAst.ParamBlock != null) {
             return scriptBlockAst.ParamBlock.Extent;
+        }
+
+        // Find the usage of the $PSCmdlet object
+
+        if (scriptBlockAst.Find(ast => ast is CommandAst commandAst
+            && (string)commandAst.CommandElements[0].SafeGetValue() == "Invoke-RunMain", false)
+            is CommandAst invokeRunMain
+        ) {
+            // If there are named arguments look for the -Cmdlet parameter, otherwise get the first argument
+            var cmdletArgument = invokeRunMain.Find(ast => ast is CommandParameterAst commandParameterAst && commandParameterAst.ParameterName == "Cmdlet", false)
+                ?? invokeRunMain.CommandElements[1];
+
+            return cmdletArgument.Extent;
         }
 
         var lastUsing = scriptBlockAst.FindAll(ast => ast is UsingStatementAst, false).LastOrDefault();
