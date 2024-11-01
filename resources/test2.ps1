8183a755d4e62340a9a1274696ff94bf802c5fde
DaRacci <me@racci.dev>
2024-11-01 18:31:30 +1100

    refactor: Rename schemas files to has suffixed with .schema.jsonc

--- resources/test2.ps1
+++ resources/test2.ps1
@@ -1,11 +1,20 @@
 Using module ../src/common/Environment.psm1
 Using module ../src/common/Logging.psm1
 Using module ../src/common/Exit.psm1
+Using module ../src/common/Analyser.psm1
 
 [CmdletBinding()]
 param()
 
 Invoke-RunMain $PSCmdlet {
+    [Compiler.Analyser.SuppressAnalyserAttribute(
+        'UseOfUndefinedFunction',
+        'thiscommand'
+    )]
+    param()
+
     Write-Error 'This is an error message!' -Category InvalidOperation;
     Invoke-FailedExit 1050;
+
+    thiscommand;
 }
