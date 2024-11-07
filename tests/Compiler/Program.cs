69739a3e494f2d09383f1106de30c74c4ecbc74d
DaRacci <me@racci.dev>
2024-11-07 18:55:42 +1100

    fix: Fix crash while outputting file and implement additional tests

--- tests/Compiler/Program.cs
+++ tests/Compiler/Program.cs
@@ -1,8 +1,10 @@
 // Copyright (c) James Draycott. All Rights Reserved.
 // Licensed under the GPL3 License, See LICENSE in the project root for license information.
 
+using System.Collections;
 using System.Globalization;
 using System.Text;
+using Moq;
 
 namespace Compiler.Test;
 
@@ -68,9 +70,101 @@ public class ProgramTests {
             Assert.That(Directory.GetFiles(outputPath), Is.Empty);
         }
     }
+
+    [Test]
+    public void Output_ToFile(
+        [Values(false, true)] bool overwrite,
+        [Values("FooBar", null)] string? expectedContent = default
+    ) {
+        const string content = "Hello World";
+        expectedContent ??= content;
+
+        var sourceDirectory = TestUtils.GenerateUniqueDirectory();
+        var outputDirectory = TestUtils.GenerateUniqueDirectory();
+        var sourceFile = TestUtils.GenerateUniqueFile(sourceDirectory, ".ps1", true, content);
+
+        var relativePath = Path.GetRelativePath(sourceDirectory, sourceFile);
+        var outputPath = Path.Combine(outputDirectory, relativePath);
+
+        // Mock the input stream to always return 'n' for overwrite.
+        var inputStream = new Mock<TextReader>();
+        inputStream.Setup(x => x.ReadLine()).Returns("n");
+        Console.SetIn(inputStream.Object);
+
+        Program.EnsureDirectoryStructure(sourceDirectory, outputDirectory, [sourceFile]);
+        Program.Output(sourceDirectory, outputDirectory, sourceFile, content, overwrite);
+        Program.Output(sourceDirectory, outputDirectory, sourceFile, expectedContent, overwrite);
+
+        Assert.Multiple(() => {
+            Assert.That(File.Exists(outputPath), Is.True);
+            Assert.That(File.ReadAllText(outputPath), Is.EqualTo(overwrite ? expectedContent : content));
+        });
+    }
+
+    [Test]
+    public void Output_ToFileOverwrites() {
+        const string content = "Hello World";
+        var sourceDirectory = TestUtils.GenerateUniqueDirectory();
+        var outputDirectory = TestUtils.GenerateUniqueDirectory();
+        var sourceFile = TestUtils.GenerateUniqueFile(sourceDirectory, ".ps1", true, content);
+
+        var relativePath = Path.GetRelativePath(sourceDirectory, sourceFile);
+        var outputPath = Path.Combine(outputDirectory, relativePath);
+
+        // Mock the input stream to always return 'y' for overwrite.
+        var inputStream = new Mock<TextReader>();
+        inputStream.Setup(x => x.ReadLine()).Returns("y");
+        Console.SetIn(inputStream.Object);
+
+        Program.EnsureDirectoryStructure(sourceDirectory, outputDirectory, [sourceFile]);
+        Program.Output(sourceDirectory, outputDirectory, sourceFile, "FooBar", true);
+        Program.Output(sourceDirectory, outputDirectory, sourceFile, content, false);
+
+        Assert.Multiple(() => {
+            Assert.That(File.Exists(outputPath), Is.True);
+            Assert.That(File.ReadAllText(outputPath), Is.EqualTo(content));
+        });
+    }
+
+    [Test]
+    public void Output_ToConsole() {
+        const string content = "Hello World";
+        var sourceDirectory = TestUtils.GenerateUniqueDirectory();
+        var sourceFile = TestUtils.GenerateUniqueFile(sourceDirectory, ".ps1", true);
+
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        Program.Output(sourceDirectory, null, sourceFile, content, false);
+        Assert.That(writer.ToString(), Is.EqualTo(content));
+    }
 }
 
 file static class TestData {
+    public static IEnumerable OutputData {
+        get {
+            var sourceDirectory = TestUtils.GenerateUniqueDirectory();
+            var outputDirectory = TestUtils.GenerateUniqueDirectory();
+            var sourceFile = TestUtils.GenerateUniqueFile(sourceDirectory, ".ps1", true);
+
+            yield return new TestCaseData(sourceDirectory, null, sourceFile, false)
+                .SetDescription("Output to console");
+
+            yield return new TestCaseData(sourceDirectory, outputDirectory, sourceFile, false)
+                .SetDescription("Output to file without overwrite");
+
+
+            var outputFile = Path.Combine(outputDirectory, Path.GetRelativePath(sourceDirectory, sourceFile));
+            File.WriteAllText(outputFile, "Foo Bar");
+
+            yield return new TestCaseData(sourceDirectory, outputDirectory, sourceFile, false, "Foo Bar")
+                .SetDescription("Output to file without overwrite");
+
+            yield return new TestCaseData(sourceDirectory, outputDirectory, sourceFile, true)
+                .SetDescription("Output to file with overwrite");
+        }
+    }
+
     public static ValueTuple<string, string[]> GenerateTestFiles(int numberofFiles, int nestedLevel, bool includeSomeIgnoredFiles) {
         var random = TestContext.CurrentContext.Random;
         var rootPath = TestUtils.GenerateUniqueDirectory();
