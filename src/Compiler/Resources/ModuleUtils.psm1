7d79be2868946c06de202c6ae50183e11cbf4eef
DaRacci <me@racci.dev>
2025-02-03 19:15:23 +1100

    fix(ScriptTemplate): Use UTF8Bom for PS < 6
    Fixes Unicode characters in Windows PS.
    Also ensures that logging preferences and other cmdletbinding parameters are properly passed through to the job.

--- src/Compiler/Resources/ModuleUtils.psm1
+++
@@ -1,1 +0,0 @@
-../../common/ModuleUtils.psm1
--- src/Compiler/Resources/ScriptTemplate.ps1
+++ src/Compiler/Resources/ScriptTemplate.ps1
@@ -10,7 +10,7 @@ begin {
     <#!DEFINE EMBEDDED_MODULES#>
     <#!DEFINE IMPORT_ORDER#>
 
-    [String]$Local:PrivatePSModulePath = $env:ProgramData | Join-Path -ChildPath 'AMT/PowerShell/Modules';
+    [String]$Local:PrivatePSModulePath = $env:ProgramData | Join-Path -ChildPath "AMT/PowerShell/Modules/PS$($PSVersionTable.PSVersion.Major)";
     if (-not (Test-Path -Path $Local:PrivatePSModulePath)) {
         Write-Verbose "Creating module root folder: $Local:PrivatePSModulePath";
         New-Item -Path $Local:PrivatePSModulePath -ItemType Directory | Out-Null;
@@ -19,6 +19,11 @@ begin {
     if (-not ($Env:PSModulePath -like "*$Local:PrivatePSModulePath*")) {
         $Env:PSModulePath = "$Local:PrivatePSModulePath;" + $Env:PSModulePath;
     }
+
+    # Must use UTF-8 Bom for PS < 6 to properly handle Unicode characters.
+    $Local:PSBelow6 = $PSVersionTable.PSVersion.Major -lt 6;
+    $Local:Bom = [Byte[]](0xEF, 0xBB, 0xBF);
+    $Local:Encoding = 'UTF8';
 
     $Script:ScriptPath;
     $Script:EMBEDDED_MODULES | ForEach-Object {
@@ -42,10 +47,24 @@ begin {
             'UTF8String' {
                 $Local:FileSuffix = if ($null -eq $Script:ScriptPath) { 'ps1' } else { 'psm1' };
                 $Local:InnerModulePath = Join-Path -Path $Local:ModuleFolderPath -ChildPath "$Local:NameHash.$Local:FileSuffix";
+
                 if (-not (Test-Path -Path $Local:InnerModulePath)) {
                     Write-Verbose "Writing content to module file: $Local:InnerModulePath"
-                    Set-Content -Path $Local:InnerModulePath -Value $Content;
+                    Set-Content -Path $Local:InnerModulePath -Value $Content -Encoding $Local:Encoding;
+                } else {
+                    $Local:Params = @{ Path = $Local:InnerModulePath; TotalCount = $Local:Bom.Length; };
+                    if ($Local:PSBelow6) { $Local:Params.Add('Encoding', 'Byte'); } else { $Local:Params.Add('AsByteStream', $True); }
+
+                    $Local:WantBom = $Local:PSBelow6
+                    $Local:IsBomEncoded = [Collections.Generic.SortedSet[String]]::CreateSetComparer().Equals((Get-Content @Local:Params), $Local:Bom);
+
+                    if ($Local:WantBom -ne $Local:IsBomEncoded) {
+                        Write-Debug "Replacing module to ensure correct UTF-8 encoding: $Local:InnerModulePath"
+
+                        Set-Content -Path $Local:InnerModulePath -Value $Content -Encoding $Local:Encoding -Force;
+                    }
                 }
+
                 if ($null -eq $Script:ScriptPath) {
                     $Script:ScriptPath = $Local:InnerModulePath;
                 }
@@ -73,7 +92,17 @@ begin {
 }
 process {
     try {
-        $Result = Start-Job { & $Using:ScriptPath @Using:PSBoundParameters } | Receive-Job -Wait -AutoRemoveJob;
+        $Result = Start-Job {
+            $VerbosePreference = $Using:VerbosePreference;
+            $DebugPreference = $Using:DebugPreference;
+            $InformationPreference = $Using:InformationPreference;
+            $WarningPreference = $Using:WarningPreference;
+            $ErrorActionPreference = $Using:ErrorActionPreference;
+            $WhatIfPreference = $Using:WhatIfPreference;
+            $ConfirmPreference = $Using:ConfirmPreference;
+
+            & $Using:ScriptPath @Using:PSBoundParameters
+        } | Receive-Job -Wait -AutoRemoveJob;
     } finally {
         $Env:PSModulePath = ($Env:PSModulePath -split ';' | Select-Object -Skip 1) -join ';';
         $Script:REMOVE_ORDER | ForEach-Object { Get-Module -Name $_ | Remove-Module -Force; }
