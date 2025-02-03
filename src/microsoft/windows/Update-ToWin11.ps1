f5ba6dce6314baafbf6337b95e4e9b71ce3ae498
DaRacci <me@racci.dev>
2025-02-03 14:01:08 +1100

    feat(scripts): Add script and module for upgrading to Windows 11

---
+++ src/microsoft/windows/Update-ToWin11.ps1
@@ -0,0 +1,23 @@
+#Requires -RunAsAdministrator
+
+Using module ..\..\common\Environment.psm1
+Using module .\Update-ToWin11.psm1
+
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [ValidateNotNull()]
+    [Switch]$CheckOnly,
+
+    [Parameter()]
+    [ValidateNotNull()]
+    [Switch]$AlwaysShowResults = $CheckOnly
+)
+
+Invoke-RunMain $PSCmdlet {
+    if (-not (Test-CanUpgrade -AlwaysShowResults:$AlwaysShowResults) -or $CheckOnly) {
+        return;
+    }
+
+    Update-ToWin11;
+}
