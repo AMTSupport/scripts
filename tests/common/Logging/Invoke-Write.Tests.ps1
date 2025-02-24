c45ccf3446768448707881c69e3bb5da3fedbdde
DaRacci <me@racci.dev>
2025-02-24 18:06:12 +1100

    chore: misc test stuff

--- tests/common/Logging/Invoke-Write.Tests.ps1
+++ tests/common/Logging/Invoke-Write.Tests.ps1
@@ -1,6 +1,31 @@
 BeforeDiscovery { Import-Module -Name "$PSScriptRoot/../../../src/common/Logging.psm1" }
 AfterAll { Remove-Module Logging }
 
+BeforeAll {
+    function Get-ShouldBeString([String]$String) {
+        $FixedString = $String -replace "`n", "`n+ ";
+
+        InModuleScope Logging {
+            if (Test-SupportsUnicode) {
+                # There is an extra space at the end of the string
+                $FixedString = " $FixedString"
+            }
+        }
+
+        return $FixedString;
+    }
+
+    function Get-Stripped([Parameter(ValueFromPipeline)][String]$String) {
+        # Replace all non-ASCII characters with a nothing string
+        # Replace all ANSI escape sequences with a nothing string
+        $String -replace '[^\u0000-\u007F]', '' -replace '\x1B\[[0-9;]*m', '';
+    }
+
+    $Params = @{
+        Message   = 'Test message'
+    };
+}
+
 Describe 'Invoke-Write Tests' {
     It 'Should not write anything if $ShouldWrite is $false' {
         @{
